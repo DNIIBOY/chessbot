22af1643f5e1892acca657ca0c381eadb3e34bd7
DNIIBOY <daniel@nettelfield.com>
2023-02-14 05:19:13 +0100

    Chessclicker also working as black

--- chessclicker.py
+++ chessclicker.py
@@ -16,20 +16,24 @@ class ChessClicker:
     def _calculate_coords(self, board_location: pyscreeze.Box):
         square_width = board_location.width / 8
         print(square_width)
-        self.file_coords = [board_location.left + i * square_width + square_width/2 for i in range(8)]
-        self.rank_coords = [board_location.top + i * square_width + square_width/2 for i in range(8)][::-1]  # Ranks start from bottom
-        # self.file_coords = [i+square_width/2 for i in self.file_coords]  # Center of each file
-        # self.rank_coords = [i+square_width/2 for i in self.rank_coords]  # Center of each rank
+        self.file_coords = [board_location.left + i * square_width + square_width / 2 for i in range(8)]
+        self.rank_coords = [board_location.top + i * square_width + square_width / 2 for i in range(8)][::-1]  # Ranks start from bottom
+        if not self.is_white:
+            # If the clicker is black, everything is reversed
+            self.file_coords = self.file_coords[::-1]
+            self.rank_coords = self.rank_coords[::-1]
         print(self.file_coords)
         print(self.rank_coords)
 
     def detect_board(self):
-        for i, img in enumerate(["img/as_white.png", "img/as_black.png"]):
+        is_white = True
+        for img in ["img/as_white.png", "img/as_black.png"]:
             if coords := pyautogui.locateOnScreen(img, confidence=0.8):
                 print(coords)
-                self.is_white = i % 2  # True first time, False second
+                self.is_white = is_white  # True first time, False second
                 self._calculate_coords(coords)
                 return coords
+            is_white = not is_white
         return []
 
     def make_move(self, from_square: str, to_square: str):
@@ -48,7 +52,7 @@ def main():
     cc = ChessClicker()
     sleep(1)
     cc.detect_board()
-    cc.make_move("e2", "e4")
+    cc.make_move("e7", "e5")
 
 
 if __name__ == '__main__':
