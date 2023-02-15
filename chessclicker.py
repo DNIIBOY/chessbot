7924025caf9eb577cadbd7abe2087a00a2983297
DNIIBOY <daniel@nettelfield.com>
2023-02-15 02:39:32 +0100

    Detect color of active squares in chessclicker.py

--- chessclicker.py
+++ chessclicker.py
@@ -1,7 +1,5 @@
 import chess
 import pyautogui
-import PIL
-import cv2
 from time import sleep
 
 import pyscreeze
@@ -11,19 +9,32 @@ class ChessClicker:
     def __init__(self):
         self.file_coords = []  # Coordinates of center of each file, len=8
         self.rank_coords = []  # Coordinates of center of each rank, len=8
+        self.offset_size = 0  # Distance in x and y direction from top left corner of square to store coordinate
         self.is_white = None  # Is the clicker playing for white?
+        self.active_colors = [(0, 0, 0), (0, 0, 0)]  # RGB values of the color of an active square on both dark and light squares
+        self.last_move = [0, 0]  # From and to square, of the last move made
 
     def _calculate_coords(self, board_location: pyscreeze.Box):
         square_width = board_location.width / 8
-        print(square_width)
-        self.file_coords = [board_location.left + i * square_width + square_width / 2 for i in range(8)]
-        self.rank_coords = [board_location.top + i * square_width + square_width / 2 for i in range(8)][::-1]  # Ranks start from bottom
+        self.offset_size = square_width / 2
+        self.file_coords = [board_location.left + i * square_width + self.offset_size for i in range(8)]
+        self.rank_coords = [board_location.top + i * square_width + self.offset_size for i in range(8)][::-1]  # Ranks start from bottom
+        self.offset_size -= 5  # Subtract 1, to ensure reversing this stays within the right square
         if not self.is_white:
             # If the clicker is black, everything is reversed
             self.file_coords = self.file_coords[::-1]
             self.rank_coords = self.rank_coords[::-1]
-        print(self.file_coords)
-        print(self.rank_coords)
+
+    def _detect_active_colors(self):
+        for square in [0, 1]:
+            file_coord = self.file_coords[chess.square_file(square)]
+            rank_coord = self.rank_coords[chess.square_rank(square)]
+            pyautogui.click(file_coord, rank_coord)  # Click the square, to give it active background
+            image = pyautogui.screenshot()
+            # Check top left corner of each square, to get only the background color
+            color = image.getpixel((file_coord - self.offset_size, rank_coord - self.offset_size))
+            self.active_colors[square] = color
+            sleep(0.1)
 
     def detect_board(self):
         is_white = True
@@ -32,9 +43,17 @@ class ChessClicker:
                 print(coords)
                 self.is_white = is_white  # True first time, False second
                 self._calculate_coords(coords)
+                self._detect_active_colors()
                 return coords
             is_white = not is_white
         return []
+
+    def detect_move(self):
+        return
+        for file in self.file_coords:
+            for rank in self.rank_coords:
+                file -= self.offset_size
+                rank -= self.offset_size
 
     def make_move(self, from_square: str, to_square: str):
         if not (self.file_coords and self.rank_coords):
@@ -46,13 +65,14 @@ class ChessClicker:
             rank_coord = self.rank_coords[chess.square_rank(square)]
             pyautogui.click(file_coord, rank_coord)
             sleep(0.1)
+        self.last_move = [from_square, to_square]
 
 
 def main():
     cc = ChessClicker()
     sleep(1)
     cc.detect_board()
-    cc.make_move("e7", "e5")
+    # cc.make_move("e7", "e5")
 
 
 if __name__ == '__main__':
